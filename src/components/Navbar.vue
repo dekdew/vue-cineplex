476bf981c9edb26d991d34a72023421718296ba4
khingbmc <ratanon.jan@gmail.com>
2019-02-26 20:26:39 +0700

    add login and sign-in

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,12 +16,25 @@
       <div class="navbar-end">
         <div class="navbar-item">
           <div class="buttons">
-            <router-link to="/signup" class="button is-info">
+            <!-- <router-link to="/signup" class="button is-info">
               <strong>Sign up</strong>
-            </router-link>
-            <router-link to="/login" class="button">
-              Log in
-            </router-link>
+            </router-link> -->
+            <section>
+              <button class="button is-info is-medium signin"
+            @click="Sign_in()">
+            <strong>Sign up</strong>
+            
+        </button>
+
+         <button class="button is-medium login"
+            @click="Login()">
+            <strong>Log in</strong>
+        </button>
+         
+            </section>
+            
+            
+       
           </div>
         </div>
       </div>
@@ -38,6 +51,126 @@
       }
     }
   }
+</script>
+
+<script>
+    const LoginModalForm = {
+        props: ['email', 'password'],
+        template: `
+            <form action="">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                :value="email"
+                                placeholder="Your email"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Your password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-checkbox>Remember me</b-checkbox>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <button class="button is-info">Login</button>
+                    </footer>
+                </div>
+            </form>
+        `
+    };
+    const SigninModalForm = {
+        props: ['email', 'password'],
+        template: `
+            <form action="">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Sign up</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                :value="email"
+                                placeholder="Your email"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Your password"
+                                required>
+                            </b-input>
+                        </b-field>
+                        <b-field label="Re-Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Re-password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <button class="button is-info">Sign up</button>
+                    </footer>
+                </div>
+            </form>
+        `
+    };
+
+    export default {
+      methods: {
+            Login() {
+                this.$modal.open({
+                    parent: this,
+                    component: LoginModalForm,
+                    hasModalCard: true
+                })
+            },
+            Sign_in() {
+                this.$modal.open({
+                    parent: this,
+                    component: SigninModalForm,
+                    hasModalCard: true
+                })
+            }
+        }
+        // components: {
+        //     ModalForm
+        // },
+        // data() {
+        //     return {
+        //         isComponentModalActive: false,
+        //         formProps: {
+        //             email: 'email@example.com',
+        //             password: 'example'
+        //         },
+        //         isComponentSignActive: false
+        //     }
+        // }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
