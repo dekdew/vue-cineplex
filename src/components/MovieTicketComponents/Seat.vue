995818f9763a1a1113a0c0d8d19cb902eaf6ea45
khingbmc <ratanon.jan@gmail.com>
2019-02-27 00:13:33 +0700

    add and modify seat

--- src/components/MovieTicketComponents/Seat.vue
+++ src/components/MovieTicketComponents/Seat.vue
@@ -1,15 +1,35 @@
 <template>
-    <div class="box">
+    <div class="box" style="text-align:center;">
         <template v-for="s in seats">
-            <button class="button" :key="s.id">{{s.id}}</button>
+            <button :class="className(s)" :disabled="s.seated" @click="chooseSeat(s)" :key="s.id">{{ s.id }}
+            
+            </button>
+            <br :key="s.id" v-if="s.num==10">
+            
         </template>
+        <span> {{selectedSeat}}</span>
     </div>
 </template>
 
 <script>
-import seat from '../../src/api/seat.json'
+import seat from '../../api/seat.json'
 
   export default {
+      props:['selectedSeat'],
+      methods:{
+          className(seat){
+              const ids = this.selectedSeat.map(s=>s.id)
+              const idx = ids.indexOf(seat.id)
+
+              return[
+                  'button',
+                  {'is-danger':seat.seated, 'is-primary':idx != -1}
+              ]
+          },
+          chooseSeat(s){
+             this.$emit('chooseSeat', s)
+          }
+      },
     computed: {
       seats(){
           return seat["seat"];
@@ -17,3 +37,4 @@ import seat from '../../src/api/seat.json'
   }
 }
 </script>
+
