2f0e7a7625b3df6767d79ffcf51544f2d40a8d99
dekdew <passawit1999@gmail.com>
2019-02-27 04:24:19 +0700

    select seat

--- src/components/MovieTicketComponents/Step2.vue
+++ src/components/MovieTicketComponents/Step2.vue
@@ -4,23 +4,29 @@
       <div class="column is-three-quarters p4">
         <div v-for="row in seats.slice().reverse()" :key="row.id" class="columns has-text-centered">
           <div v-for="seat in row.seats" :key="seat.id" class="column">
-						<div class="available" v-if="seat.status" @click="clickSeat(seat)">
-							<img v-if="seat.isClick" class="seat" src="@/assets/seat/select.svg" />
-							<img v-else class="seat" :src="require('@/assets/seat/' + seat.type_id + '.svg')" />
-						</div>
-						<div v-else-if="!seat.status">
-							<img class="seat" src="@/assets/seat/reserved.svg">
-						</div>
-					</div>
+            <div class="available" v-if="seat.status" @click="clickSeat(seat)">
+              <img v-if="seat.isClick" class="seat" src="@/assets/seat/select.svg" />
+              <img v-else class="seat" :src="require('@/assets/seat/' + seat.type_id + '.svg')" />
+            </div>
+            <div v-else-if="!seat.status">
+              <img class="seat" src="@/assets/seat/reserved.svg">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="column box p3 is-one-quarters">
+      <div class="recap column box p3 is-one-quarters">
         <p class="bold">{{ movie.title }}</p>
         <p class="has-text-info">{{ booking.date }}</p>
         <p class="has-text-info">{{ booking.time }}</p>
         <p class="bold mt3">CINEMA {{ booking.cinema.theater }}</p>
         <p class="has-text-info">{{ booking.cinema.name }}</p>
-				<p>{{ selected }}</p>
+        <div class="card mt4 p2 has-text-centered">
+          <p>Selected Seat</p>
+          <div v-if="selected.length != 0" class="columns is-multiline is-centered pt1">
+            <div class="has-text-info bold column is-3" v-for="s in selected" :key="s.seat_id">{{s.seat_id}}</div>
+          </div>
+					<p v-else class="pt1">-</p>
+        </div>
       </div>
     </div>
   </div>
@@ -62,6 +68,9 @@ export default {
 }
 .available {
 	cursor: pointer;
+}
+.recap {
+	background-color: #f5f7fb;
 }
 </style>
 
