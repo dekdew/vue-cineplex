42aa3299ac1f2c7f2a37e06f624d57d8b431ef1d
dekdew <passawit1999@gmail.com>
2019-02-27 08:29:39 +0700

    step4 finished

--- src/components/MovieTicketComponents/Step4.vue
+++ src/components/MovieTicketComponents/Step4.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="my4">
-		<div v-for="seat in booking.seats" :key="seat.seat_id">
-    <div class="card p2">
-      <div class="columns is-vcentered">
-				<div class="column is-3 has-text-centered">
-				 <p class="has-text-info h1">{{ seat.seat_id }}</p>
-				 <p>CINEMA {{ booking.cinema.theater }}</p>
-				</div>
-				<div class="column left">
-					<p class="bold">{{ booking.movie.title }}</p>
-					<p>Genre: {{ booking.movie.genres[0].name }} | {{ booking.movie.runtime }} Mins</p>
-					<p class="has-text-info mt3">{{ booking.date }} | {{ booking.time }}</p>
-					<p class="has-text-info">{{ booking.cinema.name }}</p>
-				</div>
-			</div>
+    <div v-for="seat in booking.seats" :key="seat.seat_id">
+      <div class="card p2">
+        <div class="columns is-vcentered">
+          <div class="column is-3 has-text-centered">
+            <p class="has-text-info h1">{{ seat.seat_id }}</p>
+            <p>CINEMA {{ booking.cinema.theater }}</p>
+          </div>
+          <div class="column left">
+            <p class="bold">{{ booking.movie.title }}</p>
+            <p>Genre: {{ booking.movie.genres[0].name }} | {{ booking.movie.runtime }} Mins</p>
+            <p class="has-text-info mt2">{{ booking.date }} | {{ booking.time }}</p>
+            <p class="has-text-info">{{ booking.cinema.name }}</p>
+          </div>
+        </div>
+      </div>
     </div>
-		</div>
+    <div class="buttons is-centered mt3">
+      <a class="button">Send to email</a>
+      <router-link to="/" class="button is-info">Continue</router-link>
+    </div>
   </div>
 </template>
 
